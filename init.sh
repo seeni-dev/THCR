2e6d1482fb873219394b3b933917066da8eb5fa9
SeenivasanSeeni <seeni0424@gmail.com>
2018-05-16 14:11:51 +0530

    Add changes in init.sh

--- init.sh
+++ init.sh
@@ -5,13 +5,17 @@
 if [ -d Dataset ]; then
 	echo "Direcory Exists"
 else
+	#Download the files
+	url=https://raw.githubusercontent.com/Seenivasanseeni/TamilCharacterRecognition/master/Datasets/hpl-tamil-iso-char-offline-1.0.tar.gz
+	wget -P Datasets/ $url
+
 	echo "Extracting Files"
 	tar -xf Datasets/hpl-tamil-iso-char-offline-1.0.tar.gz -C .
 	echo "Rearranging Files"
 	python RearrangeData.py
 	echo "Rearrange Done"
 fi
-if [ -d tamil_dataset_offline ];then 
+if [ -d tamil_dataset_offline ];then
 
 	echo "Removing tamil_dataset_offline"
 	rm -r tamil_dataset_offline
