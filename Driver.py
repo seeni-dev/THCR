02e79deaf5ea8e57cec369e046e4864b746ad99d
SeenivasanSeeni <seeni0424@gmail.com>
2018-05-16 20:06:10 +0530

    change epoch to a single place hold

--- Driver.py
+++ Driver.py
@@ -11,20 +11,20 @@ def train(Mod,epoch):
             if(con_stat==1):
                 break
 
-
+epoch=100
 
 def trainer():
     '''This module trains the Network adn Dumps it into the Disk'''
     Mod=Model.Model()
     Mod.construct()
-    train(Mod,100)
+    train(Mod,epoch)
     Mod.save()
     Mod="Dumped"
     Mod=Model.Model()
     Mod.construct()
     Mod.restore()
     DataHandler.char_count=0
-    train(Mod,100)
+    train(Mod,epoch)
     Mod.save()
 
 trainer()
