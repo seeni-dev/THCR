b18543153a837a0ed2be4d6e5bf096c7f22ed105
SeenivasanSeeni <seeni0424@gmail.com>
2018-06-03 21:49:47 +0530

    Modify clean.sh

--- Jobs/clean.sh
+++ Jobs/clean.sh
@@ -1,4 +1,8 @@
 #This script for cleaning all input adn output job files
-echo "Cleaning the below files"
+echo "Cleaning the below run files"
 ls *.sh.[oe]*
 rm *.sh.[oe]*
+
+echo "cleaning pycache files "
+rm  ../__pycache__/ -rf
+rm ../*/__pycache__/ -rf
