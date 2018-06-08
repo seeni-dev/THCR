b26d62912ac6a1ac0015185d116cf0ddd9711594
seeni <seeni0424@gmail.com>
2018-06-07 22:46:08 -0700

    job3.sh add function for restoration and ini.sh clash

--- Jobs/job3.sh
+++ Jobs/job3.sh
@@ -2,11 +2,17 @@
 cd THCR
 
 echo "Working in " `pwd`
+#if restore is set to True dont run init.sh
+restore=$(head  -1 conf.py)
+if [ "$restore" == "restore=True" ]; then
+    echo "This pass is being run with restoring so skipping init.sh";
+else
 sh init.sh
+fi
+
+
 echo job1 done
 
 echo "Running Driver "
 python Driver.py
 echo  "Driver Ran"
-
-touch RanFlag.file
